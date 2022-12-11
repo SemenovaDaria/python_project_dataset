07627c44781166796f0ade05e18e74123186fae0
Dorass <Semenova.Daria@yahoo.com>
2022-12-11 22:47:29 +0300

    test

--- 11.py
+++ 11.py
@@ -31,3 +31,38 @@ difference('Christian Dior')
 difs = [difference('Hermes'), difference('Christian Dior'), difference('Louis Vuitton'), difference('Kering')]
 plt.bar(['Hermes', 'Christian Dior', 'Louis Vuitton', 'Kering'], difs, color="hotpink")
 st.pyplot(fig1)
+
+fig2 = plt.figure(figsize=(16,9))
+
+ax_1 = fig.add_subplot(2, 2, 1)
+ax_2 = fig.add_subplot(2, 2, 2)
+ax_3 = fig.add_subplot(2, 2, 3)
+ax_4 = fig.add_subplot(2, 2, 4)
+
+ax_1.hist(a[a["Symbol"] == "Louis Vuitton"]["Volume"], color="maroon", label="Louis Vuitton")
+ax_2.hist(a[a["Symbol"] == "Kering"]["Volume"], color="crimson", label="Kering")
+ax_3.hist(a[a["Symbol"] == "Hermes"]["Volume"], color="pink", label="Hermes")
+ax_4.hist(a[a["Symbol"] == "Christian Dior"]["Volume"], color="salmon", label="Christian Dior")
+
+ax_1.set_ylabel("Price", fontsize=12)
+ax_2.set_ylabel("Price", fontsize=12)
+ax_3.set_ylabel("Price", fontsize=12)
+ax_4.set_ylabel("Price", fontsize=12)
+
+ax_1.set_xlabel("items sold")
+ax_2.set_xlabel("items sold")
+ax_3.set_xlabel("items sold")
+ax_4.set_xlabel("items sold")
+
+ax_1.set_title("Volume of trade of Louis Vuitton")
+ax_2.set_title("Volume of trade of Kering")
+ax_3.set_title("Volume of trade of Hermes")
+ax_4.set_title("Volume of trade of Christian Dior")
+
+ax_1.grid()
+ax_2.grid()
+ax_3.grid()
+ax_4.grid()
+
+st.pyplot(fig2)
+
