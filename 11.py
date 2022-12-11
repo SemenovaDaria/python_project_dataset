c500a78a55d225a3db8a3db882fcafc575cd4c67
Dorass <Semenova.Daria@yahoo.com>
2022-12-11 22:48:08 +0300

    test

--- 11.py
+++ 11.py
@@ -15,22 +15,6 @@ sns.pairplot(a[["Symbol", "Close", "Open"]],
              height=5)
 st.pyplot(fig)
 
-fig1 = plt.figure(figsize=(16,9))
-import matplotlib.pyplot as plt
-def hermes(name):
-    return
-def difference(firm):
-    hermes(firm)
-    close = a[(a.Symbol == firm)]
-    l_h = list(close['Close'].values)
-    h_first = l_h[0]
-    h_last = l_h[-1]
-    return (h_last/h_first)
-
-difference('Christian Dior')
-difs = [difference('Hermes'), difference('Christian Dior'), difference('Louis Vuitton'), difference('Kering')]
-plt.bar(['Hermes', 'Christian Dior', 'Louis Vuitton', 'Kering'], difs, color="hotpink")
-st.pyplot(fig1)
 
 fig2 = plt.figure(figsize=(16,9))
 
