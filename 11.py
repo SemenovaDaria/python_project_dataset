ad00f5afdae7a8c22e74e7ccc636fc89242f22f3
Dorass <Semenova.Daria@yahoo.com>
2022-12-11 22:42:40 +0300

    test

--- 11.py
+++ 11.py
@@ -4,5 +4,13 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("fashion.csv")
+a = pd.read_csv("fashion.csv")
 st.title('Mid-term report')
+
+
+fig = plt.figure(dpi=150, figsize=(16,9))
+sns.pairplot(a[["Symbol", "Close", "Open"]],
+             hue='Symbol',
+             plot_kws=dict(marker="o", linewidth=1, alpha=0.5),
+             height=5)
+st.pyplot(fig)
